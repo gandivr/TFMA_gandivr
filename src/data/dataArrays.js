3ce3dc7a8348c2149ef4906f71dce2a4c2f968fd
gandivr <156057724+gandivr@users.noreply.github.com>
2024-04-21 21:16:26 -0400

    Update dataArrays.js

--- src/data/dataArrays.js
+++ src/data/dataArrays.js
@@ -43,10 +43,6 @@ export const recipes = [
     photo_url: 'https://www.texanerin.com/content/uploads/2019/06/nobake-chocolate-cookies-1-650x975.jpg',
     photosArray: [
       'https://www.texanerin.com/content/uploads/2019/06/nobake-chocolate-cookies-1-650x975.jpg',
-      "https://namelymarly.com/wp-content/uploads/2018/04/20180415_Beet_Lasagna_10.jpg",
-      'https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.al.com/home/bama-media/width600/img/news_impact/photo/burger-fijpg-57e7e5907630c2ad.jpg',
-      'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492718105/articles/2013/09/24/burger-king-s-new-french-fries-took-ten-years-to-develop/130923-gross-burger-tease_izz59e',
-      'https://aht.seriouseats.com/images/2012/02/20120221-193971-fast-food-fries-Burger-King-fries-2.jpg'
     ],
     time: '15',
     ingredients: [[0, '200ml'], [1, '5g'], [2, '300g']],
@@ -60,10 +56,7 @@ export const recipes = [
     photo_url:
       'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=768:*',
     photosArray: [
-      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=768:*',
-      'https://www.vitamix.com/media/other/images/xVitamix-Triple-Berry-Smoothie-square-crop__1.jpg.pagespeed.ic.OgTC3ILD3R.jpg',
-      'http://images.media-allrecipes.com/userphotos/960x960/3798204.jpg',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrzui8MM6W66I29VZwVvcjpGv99JW3O1owgupc3KwB65rhAyrZ'
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-how-to-make-a-smoothie-horizontal-1542310071.png?crop=0.803xw:0.923xh;0.116xw,0.00510xh&resize=768:*'
     ],
     time: '10',
     ingredients: [
@@ -80,9 +73,6 @@ export const recipes = [
     photo_url: 'https://www.texanerin.com/content/uploads/2018/06/no-bake-lactation-cookies-1-650x975.jpg',
     photosArray: [
       'https://www.texanerin.com/content/uploads/2018/06/no-bake-lactation-cookies-1-650x975.jpg',
-      'https://ichef.bbci.co.uk/news/660/cpsprodpb/B2C0/production/_106106754_vegnuggets976.jpg',
-      'https://pixel.nymag.com/imgs/daily/grub/2017/11/22/22-mcds-chicken-tenders.w330.h330.jpg',
-      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-img.health.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Flarge_16_9%2Fpublic%2Fstyles%2Fmain%2Fpublic%2Fgettyimages-508510211.jpg%3Fitok%3Dh-Uryi8r&w=400&c=sc&poi=face&q=85'
     ],
     time: '30',
     ingredients: [
@@ -108,10 +98,6 @@ export const recipes = [
       'https://www.texanerin.com/content/uploads/2018/11/pumpkin-spice-cookies-4-650x975.jpg',
     photosArray: [
       'https://www.texanerin.com/content/uploads/2018/11/pumpkin-spice-cookies-4-650x975.jpg',
-      'https://cdn.junglecreations.com/wp/junglecms/2018/07/4164c5bd-wide-thumbnail.jpg',
-      'https://pinchofyum.com/wp-content/uploads/Crunchwrap-Inside.jpg',
-      'https://monsonmadethis.com/wp-content/uploads/2017/10/IMG_20171015_161017_025-e1533869302263.jpg'
-    ],
     time: '45',
     ingredients: [
       [0, '2 tablespoons'],
@@ -138,9 +124,6 @@ export const recipes = [
     photo_url: 'https://www.texanerin.com/content/uploads/2018/01/coconut-flour-brownies-1-650x975.jpg',
     photosArray: [
       'https://www.texanerin.com/content/uploads/2018/01/coconut-flour-brownies-1-650x975.jpg',
-      'https://images-gmi-pmc.edge-generalmills.com/6fbc6859-e2b1-499d-b0fa-ada600c9cc3f.jpg',
-      'http://www.recipe4living.com/assets/itemimages/400/400/3/83c29ac7418067c2e74f31c8abdd5a43_477607049.jpg',
-      'https://www.franchisechatter.com/wp-content/uploads/2014/08/KFC-Photo-by-James.jpg'
     ],
     time: '30',
     ingredients: [
@@ -171,8 +154,6 @@ export const recipes = [
     photo_url: 'https://hips.hearstapps.com/hmg-prod/images/190307-fish-tacos-112-1553283299.jpg',
     photosArray: [
       'http://d2814mmsvlryp1.cloudfront.net/wp-content/uploads/2014/04/WGC-Fish-Tacos-copy-2.jpg',
-      'https://thecozyapron.com/wp-content/uploads/2018/03/baja-fish-tacos_thecozyapron_1.jpg',
-      'https://www.simplyrecipes.com/wp-content/uploads/2017/06/2017-07-22-FishTacos-6.jpg'
     ],
     time: '35',
     ingredients: [
@@ -199,8 +180,6 @@ export const recipes = [
       'https://tmbidigitalassetsazure.blob.core.windows.net/secure/RMS/attachments/37/1200x1200/Flavorful-Chicken-Fajitas_EXPS_GHBZ18_12540_B08_15_8b.jpg',
     photosArray: [
       'https://dadwithapan.com/wp-content/uploads/2015/07/Spicy-Chicken-Fajitas-22-1200x480.jpg',
-      'https://3.bp.blogspot.com/-X-dHj7ORF9Q/XH4ssgTuSZI/AAAAAAAAEig/E46HP9wCfdsvyJFcMTX30cw-ICep8lF9ACHMYCw/s1600/chicken-fajitas-mexican-food-id-149559-buzzerg.jpg',
-      'https://cdn-image.foodandwine.com/sites/default/files/styles/medium_2x/public/201403-xl-chipotle-chicken-fajitas.jpg?itok=ghVcI5SQ'
     ],
     time: 35,
     ingredients: [
@@ -230,8 +209,6 @@ export const recipes = [
       'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
     photosArray: [
       'https://www.tablefortwoblog.com/wp-content/uploads/2019/01/buffalo-chicken-pizza-recipe-photos-tablefortwoblog-3-500x500.jpg',
-      'http://pizzachoicema.com/wp-content/uploads/2018/08/Buffalo-Chicken-Pizza.jpg',
-      'https://static1.squarespace.com/static/565bb41ae4b0509ba9fdf769/t/5b9a8e80aa4a998b0be0fcf4/1536855690622/pizza.gif'
     ],
     time: 50,
     ingredients: [
@@ -260,9 +237,6 @@ export const recipes = [
     photo_url: 'https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg',
     photosArray: [
       "https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg",
-      'https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.al.com/home/bama-media/width600/img/news_impact/photo/burger-fijpg-57e7e5907630c2ad.jpg',
-      'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492718105/articles/2013/09/24/burger-king-s-new-french-fries-took-ten-years-to-develop/130923-gross-burger-tease_izz59e',
-      'https://aht.seriouseats.com/images/2012/02/20120221-193971-fast-food-fries-Burger-King-fries-2.jpg'
     ],
     time: '15',
     ingredients: [[0, '200ml'], [1, '5g'], [2, '300g']],
@@ -276,8 +250,6 @@ export const recipes = [
     photo_url: 'https://static01.nyt.com/images/2021/02/14/dining/carbonara-horizontal/carbonara-horizontal-square640-v2.jpg',
     photosArray: [
       'https://static01.nyt.com/images/2021/02/14/dining/carbonara-horizontal/carbonara-horizontal-square640-v2.jpg',
-      'https://www.kcet.org/sites/kl/files/styles/kl_image_large/public/thumbnails/image/square_hero_desktop_2x_sfs_spaghetti_carbonara_clr-3.jpg?itok=T-rsBDIZ',
-      'https://cdn-image.foodandwine.com/sites/default/files/HD-201104-r-spaghetti-with-anchovy.jpg'
     ],
     time: 15,
     ingredients: [
@@ -300,8 +272,6 @@ export const recipes = [
     photo_url: 'https://images8.alphacoders.com/817/817353.jpg',
     photosArray: [
       'https://previews.123rf.com/images/somegirl/somegirl1509/somegirl150900048/46103208-top-view-of-a-delicious-traditional-italian-lasagna-made-with-minced-beef-bolognese-sauce-topped-wit.jpg',
-      'https://truffle-assets.imgix.net/87f324e4-YOUTUBE-NO-TXT.00_03_19_14.Imagen_fija001.jpg',
-      'https://images4.alphacoders.com/817/817350.jpg'
     ],
     time: 60,
     ingredients: [
@@ -333,8 +303,6 @@ export const recipes = [
     photo_url: 'https://www.yummytummyaarthi.com/wp-content/uploads/2015/02/1-2.jpg',
     photosArray: [
       'https://www.yummytummyaarthi.com/wp-content/uploads/2015/02/1-2.jpg',
-      'https://d3i71xaburhd42.cloudfront.net/2b229b43d57b14751a54322872d4cbbde5b0a099/2-Figure1-1.png',
-      'https://d3i71xaburhd42.cloudfront.net/2b229b43d57b14751a54322872d4cbbde5b0a099/1-Figure2-1.png'
     ],
     time: '90',
     ingredients: [
@@ -353,8 +321,6 @@ export const recipes = [
     photo_url: 'https://myfoodstory.com/wp-content/uploads/2021/04/Easy-Chicken-Curry-for-Beginners-1.jpg',
     photosArray: [
       'https://myfoodstory.com/wp-content/uploads/2021/04/Easy-Chicken-Curry-for-Beginners-1.jpg',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShKLSZLWprlKFCvubT7ztnHS5kYPIrMBZhGQ&usqp=CAU',
-      'https://www.whiskaffair.com/wp-content/uploads/2019/06/Chicken-Curry-3-1.jpg'
     ],
     time: '45',
     ingredients: [
@@ -373,8 +339,6 @@ export const recipes = [
     photo_url: 'https://i.ytimg.com/vi/qFkjMwzSnFI/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBiuSmw-huTy6oFyguLtwJCX2gPIw',
     photosArray: [
       'https://i.ytimg.com/vi/qFkjMwzSnFI/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBiuSmw-huTy6oFyguLtwJCX2gPIw',
-      'https://images.indianexpress.com/2017/09/fish-curry-759.jpg',
-      'https://static.toiimg.com/photo/65800182.cms'
     ],
     time: '40',
     ingredients: [
@@ -393,8 +357,6 @@ export const recipes = [
     photo_url: 'https://i.ytimg.com/vi/xerpblswTmE/maxresdefault.jpg',
     photosArray: [
       'https://i.ytimg.com/vi/xerpblswTmE/maxresdefault.jpg',
-      'https://i2.wp.com/www.spiceindiaonline.com/wp-content/uploads/2018/11/IMG_1640-1.jpg',
-      'https://www.archanaskitchen.com/images/archanaskitchen/0-Archanas-Kitchen-Recipes/2019/Andhra_Style_Spicy_Mutton_Boti_Curry_Recipe-1.jpg'
     ],
     time: '80',
     ingredients: [
